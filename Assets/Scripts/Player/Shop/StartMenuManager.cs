d4953b6cbaa0be39137bf63e1dc3b78b4c1a030d
ElreyInhumano <mqgareth@gmail.com>
2025-06-12 18:47:33 -0500

    Menu de opciones con opciones de video
    Matías Quintanilla

--- Assets/Scripts/Player/Shop/StartMenuManager.cs
+++ Assets/Scripts/Player/Shop/StartMenuManager.cs
@@ -5,15 +5,18 @@ public class StartMenuManager : MonoBehaviour
     [SerializeField] GameObject startMenuPanel;
     [SerializeField] GameObject shopPanel;
     [SerializeField] GameObject controlsPanel;
+    [SerializeField] GameObject optionsPanel;
 
     private void Start()
     {
         startMenuPanel = transform.Find("StartMenuPanel").gameObject;
         shopPanel = transform.Find("ShopPanel").gameObject;
         controlsPanel = transform.Find("ControlsPanel").gameObject;
+        optionsPanel = transform.Find("OptionsPanel").gameObject;
         shopPanel.SetActive(false);
         controlsPanel.SetActive(false);
         startMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
     }
     public void PlayButton()
     {
@@ -30,10 +33,20 @@ public class StartMenuManager : MonoBehaviour
         startMenuPanel.SetActive(false);
         controlsPanel.SetActive(true);
     }
+    public void OptionsButton()
+    {
+        startMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
     public void ReturnControlsButton()
     {
         startMenuPanel.SetActive(true);
         controlsPanel.SetActive(false);
+    }
+    public void ReturnOptionsButton()
+    {
+        startMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
     }
     public void ReturnShopButton()
     {
